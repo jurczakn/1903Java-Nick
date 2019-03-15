ced8e01d28b52fce9ac7da77bb152ab3ed031eb2
@Jurczak <nickolas.jurczak@gmail.com>
2019-03-15 16:06:44 -0400

    callable

---
+++ Week2/JDBCExample/src/main/java/com/revature/CallableDriver.java
@@ -0,0 +1,31 @@
+package com.revature;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.revature.util.ConnectionFactory;
+
+public class CallableDriver {
+	
+	public static void main(String[] args) {
+		Connection conn = ConnectionFactory.getConnection();
+		String sql = "{call get_emps(?)}";
+		try {
+			conn.setAutoCommit(false);
+			CallableStatement call =  conn.prepareCall(sql);
+			call.setInt(1, 5);
+			ResultSet rsInit = call.executeQuery();
+			rsInit.next();
+			ResultSet rs = (ResultSet) rsInit.getObject(1);
+			while (((ResultSet)rs).next()){
+				System.out.println("FName: " + ((ResultSet)rs).getString(2));
+				System.out.println("LName: " + ((ResultSet)rs).getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
