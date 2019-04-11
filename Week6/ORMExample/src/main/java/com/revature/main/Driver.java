5d8bd4049656f0762694c7ac33e9bbb7053fe26c
jurczakn <nickolas.jurczak@gmail.com>
2019-04-11 16:15:50 -0400

    fixing hibernate example

--- Week6/ORMExample/src/main/java/com/revature/main/Driver.java
+++ Week6/ORMExample/src/main/java/com/revature/main/Driver.java
@@ -2,15 +2,27 @@ package com.revature.main;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
 public class Driver {
+	
+	@Autowired
+	SessionFactory sessFact;
+	
+	@Transactional
+	public void testConn() {
+		sessFact.getCurrentSession();
+	}
 
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-		SessionFactory sessFact = (SessionFactory) ac.getBean("mySessionFactory");
-		Session sess = sessFact.getCurrentSession();
+		Driver driver = ac.getBean(Driver.class);
+		driver.testConn();
 	}
 
 }
